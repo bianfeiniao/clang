595605f016301afc0ac5fc454616e8496a7e6d9e
彼岸飞鸟 <ketingjimo@163.com>
2024-09-03 17:48:11 +0800

    初始化

---
+++ len1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+
+    int dogCount = 12;
+
+    // scanf("几条狗：%d", &dogCount);
+
+    printf("我是中国人");
+
+    return 0;
+}
